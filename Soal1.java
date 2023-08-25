c267a137c764f70e48c81ce4cbf0f67b967ca238
ryandaafriza <142970555+ryandaafriza@users.noreply.github.com>
2023-08-25 22:51:21 +0700

    Add files via upload

---
+++ Soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal1;
+
+/**
+ *
+ * @author brwnnn
+ */
+
+public class Soal1{
+	public static void main(String[] args){
+		byte angka1 = 125;
+		byte angka2 = 6;
+		byte hasil = (byte)(angka1+angka2);
+		
+		System.out.println("Hasil "+hasil);
+	}
+}
---
+++ Soal2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal2;
+
+/**
+ *
+ * @author brwnnn
+ */
+public class Soal2{
+	public static void main(String[] args){
+		int i = 42;
+		String s = (i<40)?"life":(i<50)?"universe":"everything";
+		System.out.println(s);
+	}
+}
